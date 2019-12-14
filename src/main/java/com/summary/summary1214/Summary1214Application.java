8cbff1a96b5a8e0e347d4cfa4995a1921b6d4bbf
hyson666666 <2854529108@qq.cpm>
2019-12-14 11:14:47 +0800

    first

---
+++ src/main/java/com/summary/summary1214/Summary1214Application.java
@@ -0,0 +1,13 @@
+package com.summary.summary1214;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summary1214Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Summary1214Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/summary/summary1214/Summary1214ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summary.summary1214;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summary1214ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
